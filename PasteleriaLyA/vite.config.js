1ceb3c93a05ffbdffe6eb28e95c0a9b3758f8e02
Emmanuel Salinas Caballero <emmanuelsalinascaballero2004@gmail.com>
2026-01-12 12:54:57 -0600

    PWA

--- PasteleriaLyA/vite.config.js
+++ PasteleriaLyA/vite.config.js
@@ -1,11 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'vite.svg'],
+      manifest: {
+        name: 'Sistema Pastelería LyA',
+        short_name: 'LyA POS',
+        description: 'Sistema de gestión para Pastelería y Cafetería LyA',
+        theme_color: '#be185d',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait', // <--- AQUÍ ESTÁ EL CAMBIO (Antes decía 'landscape')
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
-    host: true,  // Esto es igual a '0.0.0.0', mantenlo así
-    port: 3000,  // <--- CAMBIO IMPORTANTE: Forzamos el puerto 3000
+    host: true,
+    port: 3000,
     strictPort: true,
   }
 })
